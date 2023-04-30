2692c9f3fd4bd48c6468ea106fe80d78f40fcac7
San Me Win <sanme2685@gmail.com>
2023-04-30 11:17:04 +0630

    Added relevant icon

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-function formatDate(timeStamp){
-    let date =new Date(timeStamp)
-    let hours=date.getHours();
-    let minutes =date.getMinutes();
-    if(minutes <10){
-      minutes= `0${minutes}`
+function formatDate(timeStamp) {
+    let date = new Date(timeStamp)
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
     }
-    if(hours <10){
-        hours=`0${hours}`
+    if (hours < 10) {
+        hours = `0${hours}`
     }
     let days = [
         "Sunday",
@@ -16,24 +16,27 @@ function formatDate(timeStamp){
         "Thursday",
         "Friday",
         "Saturday"
-      ];
-      let day = days[date.getDay()];
-return  `${day} ${hours}:${minutes}`
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`
 }
 
-function displayTemperature(response){
-let temperatureElement=document.querySelector("#temperature")
-let cityElement=document.querySelector("#city")
-let descriptionElement=document.querySelector("#description")
-let humidityElement=document.querySelector("#humidity")
-let windElement=document.querySelector("#wind")
-let dateElement=document.querySelector("#date")
-temperatureElement.innerHTML=Math.round(response.data.main.temp);
-cityElement.innerHTML=response.data.name;
-descriptionElement.innerHTML=response.data.weather[0].description;
-humidityElement.innerHTML=response.data.main.humidity;
-windElement.innerHTML=Math.round(response.data.wind.speed);
-dateElement.innerHTML=formatDate(response.data.dt * 1000);
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature")
+    let cityElement = document.querySelector("#city")
+    let descriptionElement = document.querySelector("#description")
+    let humidityElement = document.querySelector("#humidity")
+    let windElement = document.querySelector("#wind")
+    let dateElement = document.querySelector("#date")
+    let iconElement = document.querySelector("#icon")
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+    iconElement.setAttribute("alt",response.data.weather[0].description)
 }
 
 let apiKey = `5f472b7acba333cd8a035ea85a0d4d4c`;
