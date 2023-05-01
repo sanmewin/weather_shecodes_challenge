6fcaf9c964832f05c06c014d91d4d4d856321a25
San Me Win <sanme2685@gmail.com>
2023-05-01 22:37:36 +0630

    Add search engine

--- src/app.js
+++ src/app.js
@@ -29,16 +29,47 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind")
     let dateElement = document.querySelector("#date")
     let iconElement = document.querySelector("#icon")
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    celsiusTemperature = response.data.main.temp;
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
-    iconElement.setAttribute("alt",response.data.weather[0].description)
+    iconElement.setAttribute("alt", response.data.weather[0].description)
+}
+function searchCity(city) {
+    let apiKey = `5f472b7acba333cd8a035ea85a0d4d4c`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(displayTemperature)
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input")
+    searchCity(cityInputElement.value)
+}
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
 
-let apiKey = `5f472b7acba333cd8a035ea85a0d4d4c`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=yangon&units=metric&appid=${apiKey}`;
-axios.get(apiUrl).then(displayTemperature)
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+searchCity("Yangon")
+let celsiusTemperature = null;
+let form = document.querySelector("#search-form")
+form.addEventListener("submit", handleSubmit)
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
