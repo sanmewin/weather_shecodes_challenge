c7756e97554acd9287ca32e6a4637d86ab0c719a
San Me Win <sanme2685@gmail.com>
2023-05-03 15:51:47 +0630

    Redesign & display the forecast weather using  the API

--- src/app.js
+++ src/app.js
@@ -20,7 +20,47 @@ function formatDate(timeStamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`
 }
-
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+function displayForecast(response){
+let forecast=response.data.daily;
+let forecastElement=document.querySelector("#forecast");
+let forecastHTML=`<div class="row">`;
+forecast.forEach(function(forecastDay,index){
+    if(index < 6){
+        forecastHTML=forecastHTML+`
+    <div class="col">
+        <div class="forecast-date">
+        ${formatDay(forecastDay.dt)}
+        </div>
+        <img src=" http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            width="30">
+        <div class="forecast-temperature">
+            <span class="forecast-temperature-max">
+                ${Math.round(forecastDay.temp.max)}°
+            </span>
+            <span class="forecast-temperature-min">
+            ${Math.round(forecastDay.temp.min)}°
+            </span>
+        </div>
+    </div>
+    `;
+    }
+})
+forecastHTML=forecastHTML+`</div>`;
+forecastElement.innerHTML=forecastHTML;
+    
+}
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "197ef3a642b76eef90e131866f74a0a0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature")
     let cityElement = document.querySelector("#city")
@@ -38,7 +78,9 @@ function displayTemperature(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
     iconElement.setAttribute("alt", response.data.weather[0].description)
+    getForecast(response.data.coord)
 }
+
 function searchCity(city) {
     let apiKey = `5f472b7acba333cd8a035ea85a0d4d4c`;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
@@ -65,7 +107,7 @@ function displayCelsiusTemperature(event) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-searchCity("Yangon")
+
 let celsiusTemperature = null;
 let form = document.querySelector("#search-form")
 form.addEventListener("submit", handleSubmit)
@@ -73,3 +115,5 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+searchCity("Yangon")
+displayForecast()
